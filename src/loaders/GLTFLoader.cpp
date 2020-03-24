83676cf3e3c6cacb6dfeab3b78e433599b073619
Tiago Patrocinio <tiagodsp93@gmail.com>
2020-03-24 01:48:18 +0100

    Improvements on Vertex information loader. Fixed bugs on TexCoord and UV info load.

--- src/loaders/GLTFLoader.cpp
+++ src/loaders/GLTFLoader.cpp
@@ -1,10 +1,10 @@
 #include "GLTFLoader.h"
 
+#include "scene/resources/surface_tool.h"
+#include <math.h>
 #include <fstream>
 #include <iostream>
 #include <vector>
-
-#include "scene/resources/surface_tool.h"
 
 #include "tinygltf/tiny_gltf.h"
 
@@ -54,81 +54,115 @@ Ref<Mesh> GLTFLoader::LoadMesh(std::vector<unsigned char> membuffer) {
 
 				// NORMALS --------------------------
 				{
-					int normals_index = primitive.attributes["NORMAL"];
-					const tinygltf::Accessor &acessor = model.accessors[normals_index];
-					tinygltf::BufferView bv = model.bufferViews[acessor.bufferView];
-					std::vector<unsigned char> buffer_data = model.buffers[bv.buffer].data;
-					for (int i = 0; i < acessor.count; i++) {
-						Vector3 vertexNormal;
-						void *ptr = &buffer_data[bv.byteOffset + acessor.byteOffset + (i * 4 * 3)];
-						vertexNormal.x = *(reinterpret_cast<float*>(ptr) + 0);
-						vertexNormal.y = *(reinterpret_cast<float*>(ptr) + 1);
-						vertexNormal.z = *(reinterpret_cast<float*>(ptr) + 2);
-						std::cout << "VERTEX NORMAL [x: " << vertexNormal.x << ", y: " << vertexNormal.y << ", z: " << vertexNormal.z << "]\n";
-						st->add_normal(vertexNormal);
+					std::string key = "NORMAL";
+					std::map<std::string, int> attr = primitive.attributes;
+					int acessor_index = attr.find(key) != attr.end() ? attr[key] : -1;
+					if (acessor_index >= 0) {
+						const tinygltf::Accessor &acessor = model.accessors[acessor_index];
+						tinygltf::BufferView bv = model.bufferViews[acessor.bufferView];
+						std::vector<unsigned char> buffer = model.buffers[bv.buffer].data;
+						size_t offset = bv.byteOffset + acessor.byteOffset;
+						for (int i = 0; i < acessor.count; i++) {
+							Vector3 vertexNormal;
+							void *ptr = &buffer[offset + (i * tinygltf::GetNumComponentsInType(acessor.type) * tinygltf::GetComponentSizeInBytes(acessor.componentType))];
+							unsigned int mask = (0xFFFFFFFF >> (32 - (int)std::pow(2, 8 * tinygltf::GetComponentSizeInBytes(acessor.componentType))));
+							for (int i = 0; i < tinygltf::GetNumComponentsInType(acessor.type); i++) {
+								unsigned int bytevalue = *(reinterpret_cast<unsigned int *>(ptr) + i) & mask;
+								vertexNormal[i] = *reinterpret_cast<float *>(&bytevalue);
+							}
+							std::cout << "VERTEX NORMAL [x: " << vertexNormal.x << ", y: " << vertexNormal.y << ", z: " << vertexNormal.z << "]\n";
+							st->add_normal(vertexNormal);
+						}
 					}
 				}
 
 				// UV TEX COORDINATES --------------------------
 				{
-					int texcoord_index = primitive.attributes["TEXCOORD_0"];
-					const tinygltf::Accessor &acessor = model.accessors[texcoord_index];
-					tinygltf::BufferView bv = model.bufferViews[acessor.bufferView];
-					std::vector<unsigned char> buffer_data = model.buffers[bv.buffer].data;
-					for (int i = 0; i < acessor.count; i++) {
-						Vector2 texcoord;
-						void *ptr = &buffer_data[bv.byteOffset + acessor.byteOffset + (i * 4 * 2)];
-						texcoord.x = *(reinterpret_cast<float*>(ptr) + 0);
-						texcoord.y = *(reinterpret_cast<float*>(ptr) + 1);
-						std::cout << "TEXTURE COORD [x: " << texcoord.x << ", y: " << texcoord.y << "]\n";
-						st->add_uv(texcoord);
+					std::string key = "TEXCOORD_0";
+					std::map<std::string, int> attr = primitive.attributes;
+					int acessor_index = attr.find(key) != attr.end() ? attr[key] : -1;
+					if (acessor_index >= 0) {
+						const tinygltf::Accessor &acessor = model.accessors[acessor_index];
+						tinygltf::BufferView bv = model.bufferViews[acessor.bufferView];
+						std::vector<unsigned char> buffer = model.buffers[bv.buffer].data;
+						size_t offset = bv.byteOffset + acessor.byteOffset;
+						for (int i = 0; i < acessor.count; i++) {
+							Vector2 texcoord;
+							void *ptr = &buffer[offset + (i * tinygltf::GetNumComponentsInType(acessor.type) * tinygltf::GetComponentSizeInBytes(acessor.componentType))];
+							unsigned int mask = (0xFFFFFFFF >> (32 - (int)std::pow(2, 8 * tinygltf::GetComponentSizeInBytes(acessor.componentType))));
+							for (int i = 0; i < tinygltf::GetNumComponentsInType(acessor.type); i++) {
+								unsigned int bytevalue = *(reinterpret_cast<unsigned int *>(ptr) + i) & mask;
+								texcoord[i] = *reinterpret_cast<float *>(&bytevalue);
+							}
+							std::cout << "TEXTURE COORD [x: " << texcoord.x << ", y: " << texcoord.y << "]\n";
+							st->add_uv(texcoord);
+						}
+					}
+				}
+
+				// VERTEX COLORS --------------------------
+				{
+					std::string key = "COLOR_0";
+					std::map<std::string, int> attr = primitive.attributes;
+					int acessor_index = attr.find(key) != attr.end() ? attr[key] : -1;
+					if (acessor_index >= 0) {
+						const tinygltf::Accessor &acessor = model.accessors[acessor_index];
+						tinygltf::BufferView bv = model.bufferViews[acessor.bufferView];
+						std::vector<unsigned char> buffer = model.buffers[bv.buffer].data;
+						size_t offset = bv.byteOffset + acessor.byteOffset;
+						for (int i = 0; i < acessor.count; i++) {
+
+							Color color = { 0, 0, 0, 1 };
+							void *ptr = &buffer[offset + (i * tinygltf::GetNumComponentsInType(acessor.type) * tinygltf::GetComponentSizeInBytes(acessor.componentType))];
+							unsigned int mask = (0xFFFFFFFF >> (32 - (int)std::pow(2, 8 * tinygltf::GetComponentSizeInBytes(acessor.componentType))));
+							for (int i = 0; i < tinygltf::GetNumComponentsInType(acessor.type); i++) {
+								unsigned int bytevalue = *(reinterpret_cast<unsigned int *>(ptr) + i) & mask;
+								color.components[i] = *reinterpret_cast<float *>(&bytevalue);
+							}
+							std::cout << "VERTEX COLOR [r: " << color.r << ", g: " << color.g << ", b: " << color.b << ", a: " << color.a << "]\n";
+							st->add_color(color);
+						}
 					}
 				}
 
 				// POSITIONS -------------------
 				{
-					int position_index = primitive.attributes["POSITION"];
-					const tinygltf::Accessor &acessor = model.accessors[position_index];
-					tinygltf::BufferView bv = model.bufferViews[acessor.bufferView];
-					std::vector<unsigned char> buffer_data = model.buffers[bv.buffer].data;
-					for (int i = 0; i < acessor.count; i++) {
-						Vector3 vertexPosition;
-						void *ptr = &buffer_data[bv.byteOffset + acessor.byteOffset + (i * 4 * 3)];
-						
-						vertexPosition.x = *(reinterpret_cast<float*>(ptr) + 0);
-						vertexPosition.y = *(reinterpret_cast<float*>(ptr) + 1);
-						vertexPosition.z = *(reinterpret_cast<float*>(ptr) + 2);
-						std::cout << "VERTEX POSITION [x: " << vertexPosition.x << ", y: " << vertexPosition.y << ", z: " << vertexPosition.z << "]\n";
-						st->add_vertex(vertexPosition);
+					std::string key = "POSITION";
+					std::map<std::string, int> attr = primitive.attributes;
+					int acessor_index = attr.find(key) != attr.end() ? attr[key] : -1;
+					if (acessor_index >= 0) {
+						const tinygltf::Accessor &acessor = model.accessors[acessor_index];
+						tinygltf::BufferView bv = model.bufferViews[acessor.bufferView];
+						std::vector<unsigned char> buffer = model.buffers[bv.buffer].data;
+						size_t offset = bv.byteOffset + acessor.byteOffset;
+						for (int i = 0; i < acessor.count; i++) {
+							Vector3 vertexPosition;
+							void *ptr = &buffer[offset + (i * tinygltf::GetNumComponentsInType(acessor.type) * tinygltf::GetComponentSizeInBytes(acessor.componentType))];
+							unsigned int mask = (0xFFFFFFFF >> (32 - (int)std::pow(2, 8 * tinygltf::GetComponentSizeInBytes(acessor.componentType))));
+							for (int i = 0; i < tinygltf::GetNumComponentsInType(acessor.type); i++) {
+								vertexPosition.coord[i] = *(reinterpret_cast<float *>(ptr) + i);
+							}
+							std::cout << "VERTEX POSITION [x: " << vertexPosition.x << ", y: " << vertexPosition.y << ", z: " << vertexPosition.z << "]\n";
+							st->add_vertex(vertexPosition);
+						}
 					}
 				}
 
 				// INDICES --------------------
 				{
-					const tinygltf::Accessor &acessor = model.accessors[primitive.indices];
-					tinygltf::BufferView bv = model.bufferViews[acessor.bufferView];
-					for (int i = 0; i < acessor.count; i++) {
-						void *ptr;
-						switch (acessor.componentType) {
-							case TINYGLTF_COMPONENT_TYPE_BYTE:
-							case TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE:
-								ptr = &model.buffers[bv.buffer].data[bv.byteOffset + acessor.byteOffset + (i * 1)];
-								st->add_index(*(unsigned char *)ptr);
-								break;
-							case TINYGLTF_COMPONENT_TYPE_SHORT:
-							case TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT:
-								ptr = &model.buffers[bv.buffer].data[bv.byteOffset + acessor.byteOffset + (i * 2)];
-								st->add_index(*(unsigned short *)ptr);
-								std::cout << "VERTEX INDEX [" << *(unsigned short *)ptr << "]\n";
-								break;
-							case TINYGLTF_COMPONENT_TYPE_INT:
-							case TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT:
-							case TINYGLTF_COMPONENT_TYPE_FLOAT:
-								ptr = &model.buffers[bv.buffer].data[bv.byteOffset + acessor.byteOffset + (i * 4)];
-								st->add_index(*(unsigned int *)ptr);
-								break;
-							default:
-								break;
+					int acessor_index = primitive.indices;
+					if (acessor_index >= 0) {
+						const tinygltf::Accessor &acessor = model.accessors[acessor_index];
+						tinygltf::BufferView bv = model.bufferViews[acessor.bufferView];
+						std::vector<unsigned char> buffer = model.buffers[bv.buffer].data;
+						size_t offset = bv.byteOffset + acessor.byteOffset;
+						for (int i = 0; i < acessor.count; i++) {
+							void *ptr = &buffer[offset + (i * tinygltf::GetNumComponentsInType(acessor.type) * tinygltf::GetComponentSizeInBytes(acessor.componentType))];
+							unsigned int mask = (0xFFFFFFFF >> (32 - (int)std::pow(2, 8 * tinygltf::GetComponentSizeInBytes(acessor.componentType))));
+							std::cout << "MASK [" << mask << "]\n";
+							unsigned int value = *(reinterpret_cast<unsigned int *>(ptr)) & mask;
+							st->add_index(value);
+							std::cout << "VERTEX INDEX [" << value << "]\n";
 						}
 					}
 				}
